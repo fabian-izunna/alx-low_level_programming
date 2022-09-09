ecd4cb8e6f7458d113893b1a3b16f2e184df2a45
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-09 12:05:02 -0700

    Task 4 and 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabets
+* Return: 0 if well executed
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints numbers
+* Return: 0 if executed properly
+*/
+int main(void)
+{
+	int i;
+	for (i >= 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n')
+	return (0);
+}
