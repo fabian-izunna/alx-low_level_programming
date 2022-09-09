8d6f0de049f445794c57ab80960ff30fc5630fb6
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-08 18:30:34 -0700

    5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+* main - prints "with proper grammar, but the outcome is a piece of art,"
+* Return: 0 if it prints, non-zero otherwise
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
