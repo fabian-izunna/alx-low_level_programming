dcf9096ab16991d153890a00e701889d4723c914
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-08 17:48:12 -0700

    3 and 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - prints "Programming is like building a multilingual puzzle
+* Return: 0 if it prints, non-zero otherwise
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
