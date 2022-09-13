2726f80195e51b7070056c921f412641fc3a1a45
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-13 12:04:50 -0700

    Task 6 and 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _abs - makes absolute numbers
+* @n: int input
+* Return: 0
+*/
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit
+* @n: int input
+* Return: 0
+*/
+int print_last_digit(int n)
+{
+	int last;
+	last = n % 10;
+	_putchar(res + '0');
+	return (last);
+}
