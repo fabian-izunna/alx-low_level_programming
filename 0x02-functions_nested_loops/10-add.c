e6d30d219a736762f140bb4ddc1493564f908db0
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-13 13:23:48 -0700

    Task 10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - adds num
+* @c: holds the result
+* Return: result
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - prints natural numbers
+* Return: void
+*/
+void print_to_98(int n)
+{
+	if ((n > 98) || (n == 98))
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+			n--;
+		}
+	}
+	else if ((n < 98) || (n == 98))
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d, ", n);
+			n++;
+		}
+	}
+	putchar(10);
+}
