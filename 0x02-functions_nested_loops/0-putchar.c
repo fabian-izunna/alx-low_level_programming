6513931005b5225504d135005c118ef1b43e7a55
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-13 06:00:54 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*printer - prints whether a number is positive or negative
+* Return: 0 if exited properly 
+*/
+int printer(void)
+{
+	char x[] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(x[i]);
+		_putchar('\n');
+	}
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - prints whether a number is positive or negative
+* Return: 0 if exited properly non-zero otherwise
+*/
+int main(void)
+{
+	return (0);
+}
