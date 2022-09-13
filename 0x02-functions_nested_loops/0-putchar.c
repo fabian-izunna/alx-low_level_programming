3e57291884c49d1aa87badf2c6eebf1ef3fbd967
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-13 08:31:56 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,12 @@
+#include <stdio>
 #include "main.h"
-/**
-*printer - prints whether a number is positive or negative
-* Return: 0 if exited properly 
-*/
-void printer(void)
-{
-	char x[] = "_putchar";
-	int i = 0;
-	while (i < 8)
-	{
-		_putchar(x[i]);
-		_putchar('\n');
-	}
-}
 
+/**
+* main - Prints something
+* Return: 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-
-/**
-* main - prints whether a number is positive or negative
-* Return: 0 if exited properly non-zero otherwise
-*/
-int main(void)
-{
-	return (0);
-}
-
-int _putchar(char c)
+int _putchar(char c);
+int main(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
