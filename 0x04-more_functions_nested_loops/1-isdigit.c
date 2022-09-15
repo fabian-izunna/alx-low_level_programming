beb14771da1ed907ea273c8ceb79a3018be4c3af
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-15 07:48:29 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - heks letters
+* @c: ithe integer
+* Return: o
+*/
+int _isupper(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
