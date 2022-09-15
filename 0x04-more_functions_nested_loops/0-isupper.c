9af2604fdaf05a78f7d8908ca2f202e8d14a5327
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-15 07:43:51 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - heks letters
+* @c: ithe integer
+* Return: o
+*/
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
