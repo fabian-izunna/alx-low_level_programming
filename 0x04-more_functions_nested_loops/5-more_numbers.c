36a25db3a3c959db0354c034c0940c1655180071
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-15 08:39:52 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers
+* Return: void
+*/
+void more_numbers(void);
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		char num = 0;
+
+		while (alpha <= 14)
+		{
+			_putchar(num + '0');
+			num++;
+		}
+	_putchar(10);
+	i++;
+	}
+}
