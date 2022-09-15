7eb49253dc40a84dfdfa67719d250ff538fead70
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-15 09:40:05 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - prints line
+* @n: input
+* Return: void
+*/
+void print_line(int n)
+{
+	int line = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	else
+		_putchar('\n');
+	_putchar('\n');
+}
