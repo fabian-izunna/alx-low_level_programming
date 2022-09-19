0e6f8dbe95a038f0e336ba41bcc6b5625e3f29cf
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-19 13:01:07 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_array - print `n` elements
+* @a: int pointer
+* @n: int var
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
