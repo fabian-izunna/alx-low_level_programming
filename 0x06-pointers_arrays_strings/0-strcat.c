03cad7a67240f8ce35ec7f28ac60318b221ae488
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-22 09:55:13 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates strings
+* @src: the source string
+* @dest: the destiation string
+* Return: the str
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = -1;
+	int j = 0;
+
+	for ( j = 0; dest[j] != '\0'; j++)
+		;
+	do {
+		i++;
+		dest[j] = src[a];
+		j++;
+	} while (src[a] != '\0');
+	return (dest);
+}
+
