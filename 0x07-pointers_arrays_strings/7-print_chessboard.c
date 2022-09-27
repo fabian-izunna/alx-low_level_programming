39750357ec2db992d83db3a3e654416c1b3fa284
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-27 04:46:27 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,12 @@
 void print_chessboard(char (*a)[8])
 {
 	int i;
-	int j;
+	int c;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+			_putchar(i[j][c]);
 		_putchar('\n');
 	}
+}
