2483e1c7c29fab2e5ae37190442fd77b75d24dd5
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-29 12:16:56 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,10 @@
 */
 int is_prime_number(int n)
 {
-	if (n % n == 0 && n % 1 == 0 && n / 1 == n)
-		return (1);
-	else if ( n <= 1)
+	if (n <= 1)
 		return (0);
+	else if (n % n == 0 && n % 1 == 0 && n / 1 == n)
+		return (1);
 	else
 		return (0);
 	return (0);
