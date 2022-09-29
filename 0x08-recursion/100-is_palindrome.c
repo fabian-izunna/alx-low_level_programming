8bb7b313587bba3155f8579b2ff37360dcff35c2
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-29 12:32:20 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* is_palindrome - shows if a str is emkpty
+* @s: the strr
+* Return: 1 if emty else 0
+*/
+int is_palindrome(char *s)
+{
+	if (*s == '\0' || *s == 0)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
