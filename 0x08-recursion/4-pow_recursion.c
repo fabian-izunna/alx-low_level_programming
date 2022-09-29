4f5e48191941f712db2eb84ed1456ab0e5e27799
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-28 22:10:57 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _pow_recursion - ploower func
+* @x: base no
+* @y: the powewr
+* Return: the ans
+*/
+int _pow_recursion(int x, int y)
+{
+	int ans;
+
+	if (y == 1)
+		return (x);
+	if (y > 1)
+	{
+		ans = x * _pow_recursion(x, (y - 1));
+		return (ans);
+	}
+	if (y == 1)
+		return (x);
+	if (y < 0)
+		return (-1);
+	return (0);
+}
