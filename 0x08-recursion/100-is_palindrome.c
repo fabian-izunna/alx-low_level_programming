e17f3a39706abfad6ea8b6456610839233be54b3
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-09-29 13:19:34 -0700

    taask 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,15 +1,49 @@
 #include "main.h"
 
 /**
+* find_strlen - Returns the length of a string.
+* @s: The string to be measured.
+* Return: The len of str
+*/
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
+
+/**
+* check_palindrome - Checks if a string is a palindrome.
+* @s: The string to be checked
+* @len: The length of s.
+* @index: The index of the string to be checked.
+* Return: If the string is a palindrome - 1.
+*/
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+	return (0);
+}
+
+/**
 * is_palindrome - shows if a str is emkpty
 * @s: the strr
 * Return: 1 if emty else 0
 */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
 		return (1);
-	else
-		return (0);
-	return (0);
+	return (check_palindrome(s, len, index));
 }
