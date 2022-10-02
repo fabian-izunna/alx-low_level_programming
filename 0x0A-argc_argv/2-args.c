5d90339fb557b6d71a8c94679f68b134fe8587eb
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-10-02 16:38:12 -0700

    Arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Prints all arguments passed into main
+* @argc: the arg
+* @argv: arg vector
+* Return (0)
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints product
+* @argc: arg count
+* @argv: arg pointer
+* Return: 1
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+		printf("%d\n", j);
+	}
+	return (0);
+}
