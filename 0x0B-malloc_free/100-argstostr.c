9f44bf152ea6bcfc38445113387f0545c951cec4
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-10-13 10:23:59 -0700

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - Concatenatesstrings
+ * @ac: the no str
+ * @av: pointer to tge str
+ * Return: 0 or pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, index, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+	str = malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+		return (NULL);
+	index = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte];
+				str[index++] = av[arg][byte];
+		str[index++] = '\n';
+	}
+	str[size] = '\0';
+	return (str);
+}
