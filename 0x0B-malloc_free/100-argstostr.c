7fc131b9f22c95af9482c46848739111495b56cf
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-10-13 10:40:55 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,8 +24,8 @@ char *argstostr(int ac, char **av)
 	index = 0;
 	for (arg = 0; arg < ac; arg++)
 	{
-		for (byte = 0; av[arg][byte];
-				str[index++] = av[arg][byte];
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
 		str[index++] = '\n';
 	}
 	str[size] = '\0';
