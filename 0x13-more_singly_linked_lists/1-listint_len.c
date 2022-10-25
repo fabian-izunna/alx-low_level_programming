a74475640eec3db72390ab93444a7190128642e7
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-10-25 08:40:59 -0700

    correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,4 +4,15 @@
  * @h: pointer to the linked list
  * Return: no of elements in the list<F11>
 **/
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
 
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
