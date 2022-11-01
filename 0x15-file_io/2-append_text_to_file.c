3f614d6d8eda3d32502a5c6fa8364f6d9ae592fb
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-11-01 14:44:04 -0700

    Task 2 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: filename.
- * @text_content: content writed in the file.
+ * @text_content: added content.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
@@ -20,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
