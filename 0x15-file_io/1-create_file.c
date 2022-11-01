320fb1f0e13a4dcf7749b52c2a0ccf824fa1f1cd
fabian-izunna <fabian.i.nwangwu@gmail.com>
2022-11-01 14:24:04 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
